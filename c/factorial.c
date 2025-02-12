ff0d87d8034955b94a3676297d09b4013a5a25c9
Karki-Biplab <k.nomem13@gmail.com>
2025-02-12 10:00:00 +0545

    c: Sddsj

--- c/factorial.c
+++ c/factorial.c
@@ -1,2 +1,10 @@
-// Learning File: factorial.c
-// Created on 2025-06-15 15:54:24
+#include <stdio.h>
+int factorial(int n) {
+    if (n <= 1) return 1;
+    return n * factorial(n - 1);
+}
+int main() {
+    int num = 5;
+    printf("Factorial of %d = %d\n", num, factorial(num));
+    return 0;
+}
---
+++ c/fibonacci.c
@@ -0,0 +1,2 @@
+// Learning File: fibonacci.c
+// Created on 2025-06-15 15:55:08
