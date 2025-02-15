7d3a313fe973ebfea76e261afa7c93ecd8d32cb0
Karki-Biplab <k.nomem13@gmail.com>
2025-02-15 10:00:00 +0545

    c: No

--- c/15_gcd_lcm.c
+++ c/15_gcd_lcm.c
@@ -1,2 +1,14 @@
-// Learning File: 15_gcd_lcm.c
-// Created on 2025-06-15 15:57:16
+#include <stdio.h>
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+int main() {
+    int a = 12, b = 18;
+    printf("GCD of %d and %d: %d\n", a, b, gcd(a, b));
+    printf("LCM of %d and %d: %d\n", a, b, lcm(a, b));
+    return 0;
+}
---
+++ c/basic_array.c
@@ -0,0 +1,2 @@
+// Learning File: basic_array.c
+// Created on 2025-06-15 15:58:09
