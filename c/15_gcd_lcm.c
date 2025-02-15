0973b8b142688e1e9c45e229aadfb43aeb8bd304
Karki-Biplab <k.nomem13@gmail.com>
2025-02-15 10:00:00 +0545

    c: skj

---
+++ c/15_gcd_lcm.c
@@ -0,0 +1,2 @@
+// Learning File: 15_gcd_lcm.c
+// Created on 2025-06-15 15:57:16
--- c/fibonacci.c
+++ c/fibonacci.c
@@ -1,2 +1,13 @@
-// Learning File: fibonacci.c
-// Created on 2025-06-15 15:55:08
+#include <stdio.h>
+int fibonacci(int n) {
+    if (n <= 1) return n;
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+int main() {
+    printf("Fibonacci series: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
+    return 0;
+}
