fb6a19b995844f8a31afacfaaa27a25cfafb9e09
Karki-Biplab <k.nomem13@gmail.com>
2025-02-19 10:00:00 +0545

    c: MO sfmnsdj

--- c/array_search.c
+++ c/array_search.c
@@ -1,2 +1,19 @@
-// Learning File: array_search.c
-// Created on 2025-06-15 15:59:32
+#include <stdio.h>
+int linearSearch(int arr[], int size, int key) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == key) return i;
+    }
+    return -1;
+}
+int main() {
+    int arr[] = {5, 2, 8, 1, 9};
+    int size = 5, key = 8;
+    int result = linearSearch(arr, size, key);
+    
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    } else {
+        printf("Element not found\n");
+    }
+    return 0;
+}
---
+++ c/bubble_sort.c
@@ -0,0 +1,2 @@
+// Learning File: bubble_sort.c
+// Created on 2025-06-15 16:00:24
