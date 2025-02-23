303e760a90487204038a2276a6a984e10ad436fc
Karki-Biplab <k.nomem13@gmail.com>
2025-02-23 10:00:00 +0545

    c: Jsdba

--- c/matrix_operations.c
+++ c/matrix_operations.c
@@ -1,2 +1,25 @@
-// Learning File: matrix_operations.c
-// Created on 2025-06-15 16:02:02
+#include <stdio.h>
+void printMatrix(int mat[][3], int rows) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main() {
+    int mat1[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    int mat2[2][3] = {{7, 8, 9}, {10, 11, 12}};
+    int result[2][3];
+    
+    // Matrix addition
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            result[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+    
+    printf("Matrix Addition Result:\n");
+    printMatrix(result, 2);
+    return 0;
+}
