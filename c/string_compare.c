5b153415e2c5564e8f10d7ecc91b2f6cc57804aa
Karki-Biplab <k.nomem13@gmail.com>
2025-03-04 10:00:00 +0545

    c: Ska

--- c/string_compare.c
+++ c/string_compare.c
@@ -1,2 +1,20 @@
-// Learning File: string_compare.c
-// Created on 2025-06-15 16:03:45
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str1[50], str2[50];
+    
+    printf("Enter first string: ");
+    gets(str1);
+    printf("Enter second string: ");
+    gets(str2);
+    
+    int result = strcmp(str1, str2);
+    if (result == 0) {
+        printf("Strings are equal\n");
+    } else if (result < 0) {
+        printf("First string is smaller\n");
+    } else {
+        printf("First string is larger\n");
+    }
+    return 0;
+}
---
+++ c/string_reverse.c
@@ -0,0 +1,2 @@
+// Learning File: string_reverse.c
+// Created on 2025-06-15 16:05:34
