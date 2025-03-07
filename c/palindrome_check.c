aabe135f101b457c1606b4bd29f33a31a92b9170
Karki-Biplab <k.nomem13@gmail.com>
2025-03-07 10:00:00 +0545

    c: LAl

---
+++ c/palindrome_check.c
@@ -0,0 +1,2 @@
+// Learning File: palindrome_check.c
+// Created on 2025-06-15 16:06:05
--- c/string_reverse.c
+++ c/string_reverse.c
@@ -1,2 +1,18 @@
-// Learning File: string_reverse.c
-// Created on 2025-06-15 16:05:34
+#include <stdio.h>
+#include <string.h>
+void reverseString(char str[]) {
+    int len = strlen(str);
+    for (int i = 0; i < len/2; i++) {
+        char temp = str[i];
+        str[i] = str[len-1-i];
+        str[len-1-i] = temp;
+    }
+}
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    gets(str);
+    reverseString(str);
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
