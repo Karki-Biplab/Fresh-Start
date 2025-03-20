41866255ccffd44a256af0f78952eba7206ebff6
Karki-Biplab <k.nomem13@gmail.com>
2025-03-20 10:00:00 +0545

    c: AHjah

--- c/pointer_arithmetic.c
+++ c/pointer_arithmetic.c
@@ -1,2 +1,11 @@
-// Learning File: pointer_arithmetic.c
-// Created on 2025-06-15 16:08:05
+#include <stdio.h>
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int *ptr = arr;
+    
+    printf("Array using pointer arithmetic:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("arr[%d] = %d, *(ptr+%d) = %d\n", i, arr[i], i, *(ptr + i));
+    }
+    return 0;
+}
---
+++ c/swap_using_pointers.c
@@ -0,0 +1,2 @@
+// Learning File: swap_using_pointers.c
+// Created on 2025-06-15 16:08:36
