67e57586db873a366dd081f6e9613adf58b57ec6
Karki-Biplab <k.nomem13@gmail.com>
2025-03-24 10:00:00 +0545

    c: drgd

--- c/dynamic_memory.c
+++ c/dynamic_memory.c
@@ -1,2 +1,27 @@
-// Learning File: dynamic_memory.c
-// Created on 2025-06-15 16:09:12
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    
+    int *arr = (int*)malloc(n * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+    
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Elements: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    free(arr);
+    return 0;
+}
---
+++ c/pointer_to_pointer.c
@@ -0,0 +1,2 @@
+// Learning File: pointer_to_pointer.c
+// Created on 2025-06-15 16:10:18
