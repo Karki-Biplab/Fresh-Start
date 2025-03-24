4a102a1e4426cef1431ad54dfef84332cb6f053c
Karki-Biplab <k.nomem13@gmail.com>
2025-03-24 10:00:00 +0545

    c: Updated files

--- c/pointer_to_pointer.c
+++ c/pointer_to_pointer.c
@@ -1,2 +1,15 @@
-// Learning File: pointer_to_pointer.c
-// Created on 2025-06-15 16:10:18
+#include <stdio.h>
+int main() {
+    int num = 100;
+    int *ptr1 = &num;
+    int **ptr2 = &ptr1;
+    
+    printf("Value of num: %d\n", num);
+    printf("Value using ptr1: %d\n", *ptr1);
+    printf("Value using ptr2: %d\n", **ptr2);
+    
+    printf("Address of num: %p\n", &num);
+    printf("Value of ptr1: %p\n", ptr1);
+    printf("Value of ptr2: %p\n", ptr2);
+    return 0;
+}
